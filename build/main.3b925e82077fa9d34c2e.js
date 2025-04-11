64e5ffd082561f5d40f7e0898988fc7a34b9944b
yuyu <sishichulu@gmail.com>
2025-04-11 22:45:58 +0300

    init: start of the project

---
+++ build/main.3b925e82077fa9d34c2e.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('HELLO WORLD');
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi4zYjkyNWU4MjA3N2ZhOWQzNGMyZS5qcyIsIm1hcHBpbmdzIjoiOzs7O0FBQUFBLE9BQU8sQ0FBQ0MsR0FBRyxDQUFDLGFBQWEsQ0FBQyxDIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vZGVjaXNpb24tbWFraW5nLXRvb2wvLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ0hFTExPIFdPUkxEJyk7XHJcbiJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sInNvdXJjZVJvb3QiOiIifQ==
